7ae1518b26c79683ecca9b37b8870206d4183b09
Mikhail Barycheuski <mik.borischewskij@gmail.com>
2017-08-18 13:38:47 +0300

    Update Home.js

--- WordAddinWeb/Home.js
+++ WordAddinWeb/Home.js
@@ -11,6 +11,7 @@
             url: "//localhost:3101" ,
             success: function (data) {
             console.log(data.toString);
+                document.body.innerHTML += '<div style="position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;">Response:' +data+'</div>'
             }
         });
 
